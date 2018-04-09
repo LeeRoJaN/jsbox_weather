37e11632de8804cee8ce5c418cfed445a636331d
LeeRoJaN <2394662001@qq.com>
2018-04-09 17:06:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+$include("scripts/define")
+
+if ($app.env != $env.app) {
+  $app.openURL("jsbox://run?name=" + encodeURI($addin.current.name));
+  return;
+}
+
 var app = require('scripts/weather_jsbox');
 
 app.getLocal();
